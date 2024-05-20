4ec9d7d74b73834687fc9dfc90ec3e700b277d29
marypupupu <166315696+marypupupu@users.noreply.github.com>
2024-05-20 15:25:11 +0300

    Update scroll.js

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,17 @@
 let menu = document.querySelector('menu')
 let header = document.querySelector('.header')
+let menu_height = menu.offsetHeight;
+let header_height = header.offsetHeight;
+
+
+window.addEventListener('resize', function() {
+  menu_height = menu.offsetHeight;
+  header_height = header.offsetHeight;
+});
+
+
 document.addEventListener('scroll', function() {
-  if (window.pageYOffset >= menu.offsetHeight + header.offsetHeight) {
+  if (window.pageYOffset >= menu_height + header_height) {
     menu.classList.add('fixed')
   }
   else {
